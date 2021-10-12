07d91d79c57da49835daaf85fdb58edf1c6b9302
William Ye <33244107+zeeplo@users.noreply.github.com>
2021-10-12 15:13:44 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hey, I'm a Node.js app!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on 3000");
+});
